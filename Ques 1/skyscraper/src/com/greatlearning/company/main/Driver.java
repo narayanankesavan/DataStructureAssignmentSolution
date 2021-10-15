1842b5c74f0dc2b8f1fcedacfb773448e75cd262
narayanankesavan <87275171+narayanankesavan@users.noreply.github.com>
2021-10-15 13:34:58 +0530

    Add files via upload
    Data Structures assessment

---
+++ Ques 1/skyscraper/src/com/greatlearning/company/main/Driver.java
@@ -0,0 +1,12 @@
+package com.greatlearning.company.main;
+
+import com.greatlearning.company.model.skyscraper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		skyscraper skyscraperService = new skyscraper();
+		skyscraperService.implementSkyscraperApp();
+	}
+
+}
---
+++ Ques 2/BST/src/com/greatlearning/company/main/Driver.java
@@ -0,0 +1,22 @@
+package com.greatlearning.company.main;
+
+import com.greatlearning.compnay.model.Node;
+import com.greatlearning.compnay.service.TreeService;
+
+public class Driver {
+	 public static Node node;
+
+	public static void main(String[] args) {
+		TreeService tree = new TreeService();
+		TreeService.node = new Node(50);
+		TreeService.node.left = new Node(30);
+		TreeService.node.right = new Node(60);
+		TreeService.node.left.left = new Node(10);
+		TreeService.node.left.right = new Node(40);
+
+	        tree.convertBSTToSkewedTree(TreeService.node);
+	        tree.traverseTree(TreeService.headNode);
+
+	}
+
+}
