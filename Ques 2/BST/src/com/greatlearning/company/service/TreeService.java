0d689ee1da0a37771d322e6338bf698271f33160
narayanankesavan <87275171+narayanankesavan@users.noreply.github.com>
2021-10-15 13:42:56 +0530

    Graded_Project 3
    Data Structures assessment

---
+++ Ques 2/BST/src/com/greatlearning/company/service/TreeService.java
@@ -0,0 +1,41 @@
+package com.greatlearning.company.service;
+
+import com.greatlearning.company.model.Node;
+
+public class TreeService {
+	public static Node node, prevNode = null, headNode = null;
+
+	// to convert BST to Skewed Tree
+	public void convertBSTToSkewedTree(Node root) {
+		// check for null tree
+		if (root == null) {
+			return;
+		}
+
+		// use recursion
+		convertBSTToSkewedTree(root.left);
+		Node rightNode = root.right;
+
+		if (headNode == null) {
+			headNode = root;
+		} else {
+			prevNode.right = root;
+		}
+		root.left = null;
+		prevNode = root;
+
+		// do recursion with rightNode
+		convertBSTToSkewedTree(rightNode);
+	}
+
+	// traverse the right skewed tree using recursion
+	public void traverseTree(Node root) {
+		if (root == null) {
+			return;
+		}
+
+		System.out.print(root.value + " ");
+		traverseTree(root.right);
+	}
+
+}
